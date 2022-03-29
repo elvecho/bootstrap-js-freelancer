bcc2566d3b75ca2c64de39656d86056c36d13426
elvecho <100934190+elvecho@users.noreply.github.com>
2022-03-29 15:50:09 +0200

    finitosenzabonus

---
+++ main.js
@@ -0,0 +1,59 @@
+
+
+
+
+function prezzoTot() {
+
+    let ore = document.getElementById("hoursrequested").value;
+    console.log(ore); 
+
+    let typeofwork = document.getElementById("typeofwork").value;
+ console.log(typeofwork);
+
+let prezzo= [];
+
+ if(typeofwork == 1){
+     let prezzoBackend = ore * 20.5;
+     prezzo.push(prezzoBackend);
+     console.log(prezzoBackend);
+ }else if(typeofwork == 2){
+    let prezzofrontend = ore * 15.3;
+    prezzo.push(prezzofrontend);
+    console.log(prezzofrontend);
+ }else if(typeofwork == 3){
+let prezzoAnalisi = ore *33.6;
+prezzo.push(prezzoAnalisi);
+console.log(prezzoAnalisi);
+ }
+ let codiciPromozionali = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+ let codice = document.getElementById("dicountCode").value
+ console.log(codice);
+ let trovato=false;
+ for(let i=0; i < codiciPromozionali.length; i++){
+    
+     if(codice == codiciPromozionali[i]){
+         trovato=true;
+         
+     }
+    }
+     if(trovato == true){
+        console.log("si");
+        let prezzoTotale= prezzo/4;
+        console.log(prezzoTotale);
+        document.getElementById("demo").innerHTML ="il tuo prezzo finale è di: " + prezzoTotale;
+     }else{
+        console.log("no");
+        console.log(prezzo);
+        document.getElementById("demo").innerHTML = "il tuo prezzo finale è di: " + prezzo;
+     }
+        
+     
+
+ }
+ 
+ 
+
+
+
+
+
